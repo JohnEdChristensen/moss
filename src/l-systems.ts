aac33f213ca4f34347b0793d202ace1483f4a3ec
John Christensen <johnedchristensen@gmail.com>
2024-06-10 21:19:41 -0500

    Add actions

--- src/l-systems.ts
+++ src/l-systems.ts
@@ -1,60 +1,61 @@
-// type Unit = string
-// type Variable = Unit
-// type Constant = Unit
-//type Character = Variable | Constant
-
-
-type l_system =
-  {
-    variables: Character[]
-    , constants: Character[]
-    , axiom: Axiom
-    , rule: Rule
-  }
-
-// Algea
-
+type Character<C extends string = any> = C
 type Axiom = Character[]
-type Character = "A" | "B"
-type Rule = (n: Character[]) => Character[]
+type Rules<T extends Character> = Map<T, T[]>
+type Actions<T extends Character> = Map<T, Action>
+type Action =
+  | { type: "rotate", degrees: number }
+  | { type: "forward", distance: number }
+  | { type: "color", color: [number, number, number] }
+  | { type: "push", action: Action }
+  | { type: "pop", action: Action }
 
-const algae: l_system =
-{
-  variables: ["A", "B"],
-  constants: [],
-  axiom: ["A"],
-  rule: (n: Character[]) => {
-    return n.map((c) => c == "A" ? ["A", "B"] : ["A"]).flat() as Character[]
+type l_system<T extends Character> =
+  {
+    name: string
+    , axiom: T[]
+    , rules: Rules<T>
+    , actions?: Actions<T>
   }
 
+function evaluateRules<T extends Character>
+  (l_system: l_system<T>, depth: number, n: T[] = []): T[] {
+  const next_iter = depth == 0 ? l_system.axiom :
+    evaluateRules(l_system, depth - 1, n)
+      .map((c) => {
+        const expanded = l_system.rules.get(c)
+        return expanded === undefined ? [c] : expanded
+      })
+      .flat()
+  console.log(next_iter.join(""))
+  return next_iter
 }
 
-// // Algea
-//
-// type A = "A"
-// type B = "B"
-// type Axiom = Character[]
-// type Character = A | B
-// type Rule = (n: Character[]) => Character[]
-//
-// const algae: l_system =
-// {
-//   variables: ["A", "B"],
-//   constants: [],
-//   axiom: ["A"],
-//   rule: (n: Character[]) => {
-//     return n.map((c) => c == "A" ? ["A", "B"] : ["A"]).flat() as Character[]
-//   }
-//
-// }
-
-// Run
-
-let val: Character[] = algae.axiom
-console.log(val.join(""))
-
-for (let i = 0; i < 5; i++) {
-  val = algae.rule(val)
-  console.log(val.join(""))
+const algae: l_system<"A" | "B"> =
+{
+  name: "Algea",
+  axiom: ["A"],
+  rules: new Map([
+    ["A", ["A", "B"]],
+    ["B", ["A"]]
+  ])
 }
 
+const binaryTree: l_system<"0" | "1" | "[" | "]"> =
+{
+  name: "BinaryTree",
+  axiom: ["0"],
+  rules: new Map([
+    ["1", ["1", "1"]],
+    ["0", ["1", "[", "0", "]", "0"]],
+  ]),
+  actions: new Map([
+    ["0", { type: "forward", distance: 10 }],
+    ["1", { type: "forward", distance: 10 }],
+    ["[", { type: "push", action: { type: "rotate", degrees: 45 } }],
+    ["]", { type: "pop", action: { type: "rotate", degrees: -45 } }]
+  ])
+}
+
+const systems = [algae, binaryTree]
+evaluateRules(algae, 5)
+evaluateRules(binaryTree, 3)
